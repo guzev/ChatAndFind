df381b165d9cbf99123eecf515841683e93667b0
Zem4ik <vladbullzv@gmail.com>
2016-12-09 23:14:49 +0300

    SignInActivity changed

--- app/src/main/java/com/chatandfind/android/SignInActivity.java
+++ app/src/main/java/com/chatandfind/android/SignInActivity.java
@@ -3,10 +3,7 @@ package com.chatandfind.android;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -26,8 +23,8 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.GoogleAuthProvider;
 
-public class SingInActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
-    private static final String TAG = "SingInActivity";
+public class SignInActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+    private static final String TAG = "SignInActivity";
     private SignInButton signInButton;
     private static final int RC_SIGN_IN = 9001;
     private GoogleApiClient mGoogleApiClient;
@@ -39,6 +36,7 @@ public class SingInActivity extends AppCompatActivity implements GoogleApiClient
         setContentView(R.layout.activity_sing_in);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
@@ -55,12 +53,16 @@ public class SingInActivity extends AppCompatActivity implements GoogleApiClient
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed: " + connectionResult);
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
+            //Try to sign in throw Google account
             case R.id.sign_in_btn:
                 signIn();
                 break;
@@ -68,6 +70,7 @@ public class SingInActivity extends AppCompatActivity implements GoogleApiClient
     }
 
     private void signIn() {
+        //signing in via Google Sign In UI
         Intent singInActivity = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(singInActivity, RC_SIGN_IN);
     }
@@ -76,12 +79,14 @@ public class SingInActivity extends AppCompatActivity implements GoogleApiClient
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+        //checking result after signing in
         if (requestCode == RC_SIGN_IN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             if (result.isSuccess()) {
                 GoogleSignInAccount account = result.getSignInAccount();
                 firebaseAuthWithGoogle(account);
             } else {
+                Toast.makeText(SignInActivity.this, "Google sing in failed.", Toast.LENGTH_LONG).show();
                 Log.e(TAG, "Google sing in failed.");
             }
         }
@@ -102,10 +107,11 @@ public class SingInActivity extends AppCompatActivity implements GoogleApiClient
                         // signed in user can be handled in the listener.
                         if (!task.isSuccessful()) {
                             Log.w(TAG, "signInWithCredential", task.getException());
-                            Toast.makeText(SingInActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_LONG).show();
+                            Toast.makeText(SignInActivity.this, "Authentication failed.", Toast.LENGTH_LONG).show();
                         }
-                        // ...
+                        Toast.makeText(SignInActivity.this, "Authentication successfully finished!", Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                        finish();
                     }
                 });
     }
