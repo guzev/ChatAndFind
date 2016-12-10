51afc32614a54af4d1507ad027c9e819526fa8ce
Guzev Ivan <guzev1997@gmail.com>
2016-12-10 23:01:00 +0300

    MainActivity changes

--- app/src/main/java/com/chatandfind/android/MainActivity.java
+++ app/src/main/java/com/chatandfind/android/MainActivity.java
@@ -3,8 +3,13 @@ package com.chatandfind.android;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
@@ -52,5 +57,13 @@ public class MainActivity extends AppCompatActivity {
         e_mailView.setText(e_mail);
         if (imageURL != null) Glide.with(this).load(imageURL).into(photo);
     }
-}
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+
+}
