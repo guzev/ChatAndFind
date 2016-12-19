1be481ff2e3fc24441f5f14534bcd9f122e09503
Zem4ik <vladbullzv@gmail.com>
2016-12-19 21:15:54 +0300

    Notification changed

--- app/src/main/java/com/chatandfind/android/MessagesService.java
+++ app/src/main/java/com/chatandfind/android/MessagesService.java
@@ -10,6 +10,7 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.chatandfind.android.config.Config;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,48 +18,76 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class MessagesService extends Service {
     private final static String TAG = "MessagesService";
     NotificationManager notificationManager;
     DatabaseReference databaseReference;
     String encodedEmail;
+    ChildEventListener userChatListListener;
+    HashSet<String> stringSet;
 
     public MessagesService() {
     }
 
     @Override
     public void onCreate() {
-        Log.d(TAG, "onCreate");
         super.onCreate();
+        Log.d(TAG, "onCreate");
+        encodedEmail = Config.encodeForFirebaseKey(FirebaseAuth.getInstance().getCurrentUser().getEmail());
         notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        stringSet = new HashSet<>();
+        userChatListListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if ((long) dataSnapshot.child("lastSeenMessageTime").getValue() == (long) dataSnapshot.child("lastMessageTime").getValue()) {
+                    stringSet.remove(dataSnapshot.getKey());
+                } else {
+                    stringSet.add(dataSnapshot.getKey());
+                }
+                sendNotification();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                if ((long) dataSnapshot.child("lastSeenMessageTime").getValue() == (long) dataSnapshot.child("lastMessageTime").getValue()) {
+                    stringSet.remove(dataSnapshot.getKey());
+                } else {
+                    stringSet.add(dataSnapshot.getKey());
+                }
+                sendNotification();
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                if ((long) dataSnapshot.child("lastSeenMessageTime").getValue() != (long) dataSnapshot.child("lastMessageTime").getValue()) {
+                    stringSet.remove(dataSnapshot.getKey());
+                }
+                sendNotification();
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        };
+        databaseReference = FirebaseDatabase.getInstance().getReference().child(Config.CHAT_LIST).child(encodedEmail);
+        databaseReference.addChildEventListener(userChatListListener);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+        databaseReference.removeEventListener(userChatListListener);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(TAG, "onStartCommand");
-        if (intent != null) {
-            encodedEmail = intent.getStringExtra(Config.ENC_EMAIL_TAG);
-            databaseReference = FirebaseDatabase.getInstance().getReference().child(Config.CHAT_LIST).child(encodedEmail);
-            databaseReference.addChildEventListener(new ChildEventListener() {
-                @Override
-                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                }
-
-                @Override
-                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-                    sendNotification();
-                }
-
-                @Override
-                public void onChildRemoved(DataSnapshot dataSnapshot) {}
-
-                @Override
-                public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {}
-            });
-        }
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -68,14 +97,19 @@ public class MessagesService extends Service {
     }
 
     void sendNotification() {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                .setContentTitle("New Message")
-                .setContentText("You've received new messages.")
-                .setSmallIcon(R.drawable.ic_account_circle_black_36dp);
+        if (stringSet.size() == 0) {
+            notificationManager.cancel(0);
+        } else {
+            String contentText = "У вас новые сообщения в " + stringSet.size() + (stringSet.size()  == 1 ? " чате" : " чатах");
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                    .setContentTitle("New Messages")
+                    .setContentText(contentText)
+                    .setSmallIcon(R.drawable.ic_account_circle_black_36dp);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-        builder.setContentIntent(pendingIntent);
-        notificationManager.notify(0, builder.build());
+            Intent intent = new Intent(this, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+            builder.setContentIntent(pendingIntent);
+            notificationManager.notify(0, builder.build());
+        }
     }
 }
