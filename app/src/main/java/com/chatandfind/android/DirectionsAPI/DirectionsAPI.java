ad378fab99ae64a397aef48a5270e955efbbb159
Guzev Ivan <guzev1997@gmail.com>
2016-12-20 04:27:12 +0300

    Added walking direction with changes

--- app/src/main/java/com/chatandfind/android/DirectionsAPI/DirectionsAPI.java
+++ app/src/main/java/com/chatandfind/android/DirectionsAPI/DirectionsAPI.java
@@ -18,7 +18,7 @@ public class DirectionsAPI {
     private static String API_KEY = "AIzaSyAsrCZII1MWCOVZRtOounYSWi3mT5zZSzo";
 
     public static HttpURLConnection getDirection(double myLatitude, double myLongtitude, double destinationLatitude, double destinationLongtitude) throws IOException{
-        Uri url = (BASE_URL).buildUpon().appendQueryParameter("origin", myLatitude + "," + myLongtitude).appendQueryParameter("destination", destinationLatitude + "," + destinationLongtitude).appendQueryParameter("sensor", "false").build();
+        Uri url = (BASE_URL).buildUpon().appendQueryParameter("origin", myLatitude + "," + myLongtitude).appendQueryParameter("destination", destinationLatitude + "," + destinationLongtitude).appendQueryParameter("mode", "walking").appendQueryParameter("sensor", "false").build();
         return (HttpURLConnection) new URL(url.toString()).openConnection();
     }
 
